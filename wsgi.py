68a44a07cb05ab16c83fdf57e7bb2273db924566
Daniel Vizzini <dvizzini@gmail.com>
2012-04-26 16:27:20 -0700

    more sysadmin tweaking

--- wsgi.py
+++ wsgi.py
@@ -3,12 +3,12 @@ import site
 import os
 import sys
 
-SITE_DIR = '/opt/django-trunk/xlab_server'
+SITE_DIR = '/opt/django-trunk/'
 
 site.addsitedir(SITE_DIR) 
 
 sys.path.append(SITE_DIR)
 
-os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
+os.environ['DJANGO_SETTINGS_MODULE'] = 'xlab_server.settings'
 import django.core.handlers.wsgi
 application = django.core.handlers.wsgi.WSGIHandler()
