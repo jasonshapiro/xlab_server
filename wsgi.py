6f7b0e12c365c87db563f39390d195d33af80b56
Daniel Vizzini <dvizzini@gmail.com>
2012-05-17 18:38:05 -0700

    Sys admin headaches

--- wsgi.py
+++ wsgi.py
@@ -12,3 +12,13 @@ sys.path.append(SITE_DIR)
 os.environ['DJANGO_SETTINGS_MODULE'] = 'xlab_server.settings'
 import django.core.handlers.wsgi
 application = django.core.handlers.wsgi.WSGIHandler()
+
+import django.core.handlers.wsgi
+class ForcePostHandler(django.core.handlers.wsgi.WSGIHandler):
+    """Workaround for: http://lists.unbit.it/pipermail/uwsgi/2011-February/001395.html
+    """
+    def get_response(self, request):
+        request.POST # force reading of POST data
+        return super(ForcePostHandler, self).get_response(request)
+
+application = ForcePostHandler()
